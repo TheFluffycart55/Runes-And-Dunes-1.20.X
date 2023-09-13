d91f4ecab8adadd797e13e8fca78867ec9f3b5c6
TheFluffycart55 <apmva2003@gmail.com>
2023-09-13 15:53:02 -0400

    Test

---
+++ src/main/java/net/thefluffycart/dunes_mod/items/ModItems.java
@@ -0,0 +1,23 @@
+package net.thefluffycart.dunes_mod.items;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.thefluffycart.dunes_mod.RunesAndDunesMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, RunesAndDunesMod.MOD_ID);
+
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
