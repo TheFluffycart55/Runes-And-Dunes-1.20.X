945aa12e25997fdd770ffe776b4fa4f5732bd0fa
TheFluffycart55 <apmva2003@gmail.com>
2023-09-13 15:15:39 -0400

    Test

--- src/main/java/net/thefluffycart/dunes_mod/RunesAndDunesMod.java
+++ src/main/java/net/thefluffycart/dunes_mod/RunesAndDunesMod.java
@@ -13,13 +13,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(RunesAndDunesMod.MOD_ID)
 public class RunesAndDunesMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "dunes_mod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public RunesAndDunesMod()
